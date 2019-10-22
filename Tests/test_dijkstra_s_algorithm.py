7320cb0b06bf5a7b54b61fe34f6974a8ddace684
Fanzil <fan.hakimov2010@yandex.ru>
2019-10-22 22:00:42 +0600

    add Dijkstra's Algorithm

---
+++ Tests/test_dijkstra_s_algorithm.py
@@ -0,0 +1,95 @@
+from unittest import TestCase, main
+from dijkstra_s_algorithm import Dijkstra
+
+
+class TestDijkstra(TestCase):
+    def test_calculate(self):
+        arr = [
+            [0, 2, 3, 5, 0, 0, 4, 0],
+            [0, 0, 0, 0, 2, 0, 1, 0],
+            [0, 0, 0, 0, 3, 4, 0, 0],
+            [0, 0, 0, 0, 0, 4, 2, 0],
+            [0, 0, 0, 0, 0, 0, 0, 6],
+            [0, 0, 0, 0, 0, 0, 0, 4],
+            [0, 0, 0, 0, 0, 0, 0, 4],
+            [0 for i in range(8)]
+        ]
+        self.assertEqual(
+            Dijkstra.calculate(arr, 8),
+            "The shortest path from the start to the node 8 is "
+            "1 -> 2 -> 7 -> 8.\nThe length of the path is 7.",
+        )
+        self.assertEqual(
+            Dijkstra.calculate(arr, 7),
+            "The shortest path from the start to the node 7 is 1 -> 2 -> 7."
+            "\nThe length of the path is 3.",
+        )
+        self.assertEqual(Dijkstra.calculate(arr, 0), "The node is incorrect!")
+        self.assertEqual(Dijkstra.calculate(arr, 9), "The node is incorrect!")
+
+        arr = [
+            [0, 4, 2, 0, 0, 0],
+            [0, 0, 5, 10, 0, 0],
+            [0, 0, 0, 0, 3, 0],
+            [0, 0, 0, 0, 0, 11],
+            [0, 0, 0, 4, 0, 0],
+            [0 for i in range(6)]
+        ]
+        self.assertEqual(
+            Dijkstra.calculate(arr, 6),
+            "The shortest path from the start to the node 6 is "
+            "1 -> 3 -> 5 -> 4 -> 6.\nThe length of the path is 20.",
+        )
+
+        arr = [
+            [0, 1, 3, 4, 0, 9],
+            [0, 0, 0, 4, 0, 0],
+            [0, 0, 0, 0, 2, 5],
+            [0, 0, 0, 0, 0, 3],
+            [0, 0, 0, 0, 0, 2],
+            [0 for i in range(6)]
+        ]
+        self.assertEqual(
+            Dijkstra.calculate(arr, 6),
+            "The shortest path from the start to the node 6 is 1 -> 4 -> 6."
+            "\nThe length of the path is 7.",
+        )
+
+        arr = [
+            [0, 6],
+            [0, 0]
+        ]
+        self.assertEqual(
+            Dijkstra.calculate(arr, 2),
+            "The shortest path from the start to the node 2 is 1 -> 2."
+            "\nThe length of the path is 6.",
+        )
+
+        arr = [
+            [0, 4, 2, 0, 0, 0],
+            [0, 0, 5, 10, 0, 0],
+            [0, 0, 0, 0, 3, 0],
+            [0, 0, 0, 0, 0, 11],
+            [0, 0, 0, 4, 0, 0]
+        ]
+        self.assertEqual(
+            Dijkstra.calculate(arr, 5), "The adjacency matrix is incorrect!"
+        )
+
+        arr = [
+            [0, 6, 0],
+            [6, 0, 0],
+            [0, 0, 0]
+        ]
+        self.assertEqual(
+            Dijkstra.calculate(arr, 3),
+            "There is no path from start to the node 3."
+        )
+
+    @staticmethod
+    def run_test():
+        main()
+
+
+if __name__ == "__main__":
+    main()
---
+++ dijkstra_s_algorithm.py
@@ -0,0 +1,54 @@
+class Dijkstra:
+    """
+    Class for calculating the shortest paths from the start to the other nodes
+    in a graph.
+    """
+
+    @staticmethod
+    def calculate(arr, node: int) -> str:
+        """
+        :param arr: an adjacency matrix
+        :param node: a node to which it is necessary to find a path
+        :return: the shortest path to a node
+        """
+        n = len(arr)
+        for subarr in arr:
+            if len(subarr) != n:
+                return "The adjacency matrix is incorrect!"
+
+        node = int(node)
+        if node < 2 or node > n:
+            return "The node is incorrect!"
+
+        arrV = [999 for i in range(n)]
+        arrM = []
+
+        arrV[0] = 0
+        arrM.append(0)
+        while arrM:
+            i = arrM.pop(0)
+            arrE = [j for j in range(n) if arr[i][j]]
+            for j in arrE:
+                temp = arrV[j]
+                arrV[j] = min(arrV[j], arrV[i] + arr[i][j])
+                if arrV[j] < temp:
+                    arrM.append(j)
+
+        if arrV[node - 1] == 999:
+            return "There is no path from start to the node %d." % node
+
+        path = []
+        i = node - 1
+        while i:
+            arrRevE = [j for j in range(n) if arr[j][i]]
+            for j in arrRevE:
+                if arrV[i] - arrV[j] == arr[j][i]:
+                    path.append(str(i + 1))
+                    i = j
+                    break
+
+        return (
+            "The shortest path from the start to the node %d is 1 -> %s."
+            "\nThe length of the path is %d."
+            % (node, " -> ".join(path[::-1]), arrV[node - 1])
+        )
