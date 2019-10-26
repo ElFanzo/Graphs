1f5c64413abc908f31eee35af1f8ac14c6cab5e6
Fanzil <fan.hakimov2010@yandex.ru>
2019-10-26 23:27:39 +0600

    add lee_algorithm module import

--- run_tests.py
+++ run_tests.py
@@ -1,4 +1,6 @@
 from Tests.test_dijkstra_s_algorithm import TestDijkstra
+from Tests.test_lee_algorithm import TestAlgorithm
 
 if __name__ == "__main__":
     TestDijkstra.run_test()
+    TestAlgorithm.run_test()
